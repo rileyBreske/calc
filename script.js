63936ef34d840f1385ce90d5bee5e877b7c1d9dd
Riley Breske <rilobreske@gmail.com>
2023-03-23 14:55:25 -0400

    added functionality to google the saved number

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(display.value);
     });
 });
+
+
+function openGoogleSearch() {
+    // Get the number from the calculator
+    var number = output.value;
+
+    // Generate the Google search URL
+    var url = 'https://www.google.com/search?q=' + number;
+
+    // Open a new window with the Google search URL
+    window.open(url);
+}
+
